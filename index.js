c403637a1cb1b2cdb4df25361b59953deb52dbbf
intripida <intripida@gmail.com>
2023-05-13 20:27:57 -0500

    Fix credentials and deploy

---
+++ index.js
@@ -0,0 +1,15 @@
+var port = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send("Hellow world- project fit interview")
+});
+
+app.listen ( port, function (){
+  console.log(`Listening on port ${port}! `);
+});
+
+app.listen (port, function(){
+  console.log(`Listening on port ${port}!`)  
+});
